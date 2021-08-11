6185ffe7fb908b3e78bf7a49a03a4f268d18c20f
deok <39942335+ziinxed@users.noreply.github.com>
2021-08-12 01:20:39 +0900

    change graphqlModule Option (memory -> file)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { FooResolver } from './resolvers.foo';
+import { join } from 'path';
 
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true })],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, FooResolver, PrismaService],
 })
