a8e40e5d53aa50fdb13061e65567c632fe249283
hyemmie <lhm0710@naver.com>
2021-09-15 22:56:26 +0900

    hotfix lint

--- src/deferenceImage/deferenceImage.service.ts
+++ src/deferenceImage/deferenceImage.service.ts
@@ -1,53 +1,60 @@
-import { PrismaClient, DeferenceImage } from ".prisma/client";
-import { Injectable } from "@nestjs/common";
-import { CreateDeferenceImageInput, UpdateDeferenceImageInput } from "deferenceImage/deferenceImage.input";
+import { PrismaClient, DeferenceImage } from '.prisma/client';
+import { Injectable } from '@nestjs/common';
+import {
+  CreateDeferenceImageInput,
+  UpdateDeferenceImageInput,
+} from 'deferenceImage/deferenceImage.input';
 
 @Injectable()
 export class DeferenceImageService {
-    constructor(private prisma: PrismaClient) {}
+  constructor(private prisma: PrismaClient) {}
 
-    async getDeferenceImage(id: number): Promise<DeferenceImage> {
-        return await this.prisma.deferenceImage.findUnique({
-            where: {
-                id,
-            }
-        })
-    }
+  async getDeferenceImage(id: number): Promise<DeferenceImage> {
+    return await this.prisma.deferenceImage.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
 
-    async getDeferenceImages(): Promise<DeferenceImage[]> {
-        return await this.prisma.deferenceImage.findMany();
-    }
+  async getDeferenceImages(): Promise<DeferenceImage[]> {
+    return await this.prisma.deferenceImage.findMany();
+  }
 
-    async createDeferenceImage(createDeferenceImageInput: CreateDeferenceImageInput): Promise<DeferenceImage> {
-        const { type, image_url, deferenceId } = createDeferenceImageInput;
-        return this.prisma.deferenceImage.create({
-            data: {
-                type,
-                image_url,
-                deferenceId
-            }
-        })
-    }
+  async createDeferenceImage(
+    createDeferenceImageInput: CreateDeferenceImageInput,
+  ): Promise<DeferenceImage> {
+    const { type, image_url, deferenceId } = createDeferenceImageInput;
+    return this.prisma.deferenceImage.create({
+      data: {
+        type,
+        image_url,
+        deferenceId,
+      },
+    });
+  }
 
-    async updateDeferenceImage(updateDeferenceImageInput: UpdateDeferenceImageInput): Promise<DeferenceImage> {
-        const { id, type, image_url, deferenceId } = updateDeferenceImageInput;
-        return await this.prisma.deferenceImage.update({
-            where: {
-                id,
-            },
-            data: {
-                type,
-                image_url,
-                deferenceId
-            }
-        })
-    }
+  async updateDeferenceImage(
+    updateDeferenceImageInput: UpdateDeferenceImageInput,
+  ): Promise<DeferenceImage> {
+    const { id, type, image_url, deferenceId } = updateDeferenceImageInput;
+    return await this.prisma.deferenceImage.update({
+      where: {
+        id,
+      },
+      data: {
+        type,
+        image_url,
+        deferenceId,
+      },
+    });
+  }
 
-    async deleteDeferenceImage(id: number): Promise<DeferenceImage> {
-        return await this.prisma.deferenceImage.delete({
-            where: {
-                id,
-            }
-        })
-    }
+  async deleteDeferenceImage(id: number): Promise<DeferenceImage> {
+    return await this.prisma.deferenceImage.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
