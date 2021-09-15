e4c41f7bd8f34111afc6bfb9f8d7a907305cd1e0
hyemmie <lhm0710@naver.com>
2021-09-15 22:56:54 +0900

    add include member option

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,59 +1,73 @@
-import { News, PrismaClient } from ".prisma/client";
-import { Injectable } from "@nestjs/common";
-import { CreateNewsInput, UpdateNewsInput } from "./news.input";
+import { News, PrismaClient } from '.prisma/client';
+import { Injectable } from '@nestjs/common';
+import { CreateNewsInput, UpdateNewsInput } from './news.input';
 
 @Injectable()
 export class NewsService {
-    constructor(private prisma: PrismaClient) {}
+  constructor(private prisma: PrismaClient) {}
 
-    async getNews(id: number): Promise<News> {
-        return await this.prisma.news.findUnique({
-            where: {
-                id,
-            }
-        })
-    };
+  async getNews(id: number): Promise<News> {
+    return await this.prisma.news.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        member: true,
+      },
+    });
+  }
 
-    async getAllNews(): Promise<News[]> {
-        return await this.prisma.news.findMany();
-    }
+  async getAllNews(): Promise<News[]> {
+    return await this.prisma.news.findMany();
+  }
 
-    async createNews(createNewsInput: CreateNewsInput): Promise<News> {
-        const { title, memberId, summary, link, image_url, is_disclosed } = createNewsInput;
-        return this.prisma.news.create({
-            data: {
-                title,
-                memberId,
-                summary,
-                link,
-                image_url,
-                is_disclosed
-            }
-        });
-    }
+  async createNews(createNewsInput: CreateNewsInput): Promise<News> {
+    const { title, memberId, summary, link, image_url, is_disclosed } =
+      createNewsInput;
+    return this.prisma.news.create({
+      data: {
+        title,
+        memberId,
+        summary,
+        link,
+        image_url,
+        is_disclosed,
+      },
+      include: {
+        member: true,
+      },
+    });
+  }
 
-    async updateNews(updateNewsInput: UpdateNewsInput): Promise<News> {
-        const { id, title, memberId, summary, link, image_url, is_disclosed } = updateNewsInput;
-        return this.prisma.news.update({
-            where: {
-                id,
-            },
-            data: {
-                title, 
-                memberId,
-                summary,
-                link,
-                image_url,
-                is_disclosed
-            }
-        })
-    }
+  async updateNews(updateNewsInput: UpdateNewsInput): Promise<News> {
+    const { id, title, memberId, summary, link, image_url, is_disclosed } =
+      updateNewsInput;
+    return this.prisma.news.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        memberId,
+        summary,
+        link,
+        image_url,
+        is_disclosed,
+      },
+      include: {
+        member: true,
+      },
+    });
+  }
 
-    async deleteNews(id: number): Promise<News> {
-        return await this.prisma.news.delete({
-            where: {
-                id,
-            }
-        })
-    }
+  async deleteNews(id: number): Promise<News> {
+    return await this.prisma.news.delete({
+      where: {
+        id,
+      },
+      include: {
+        member: true,
+      },
+    });
+  }
 }
