b830961249fe27b35f67e3896bd89b11c82307b9
hyemmie <lhm0710@naver.com>
2021-09-15 18:23:30 +0900

    add period module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaService } from './prisma.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { FooResolver } from './resolvers.foo';
 import { MemberModule } from './member/member.module';
+import { PeriodModule } from './period/period.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true }), MemberModule],
+  imports: [GraphQLModule.forRoot({ autoSchemaFile: true }), MemberModule, PeriodModule],
   controllers: [AppController],
   providers: [AppService, FooResolver, PrismaService],
 })
