1004329213776cf820c68dba700eadaf713d9e06
hyemmie <lhm0710@naver.com>
2021-09-28 02:33:21 +0900

    add providers to project module

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { ProjectResolver } from './project.resolver';
 import { ProjectService } from './project.service';
 
 @Module({
-  providers: [ProjectService],
+  providers: [ProjectService, ProjectResolver, PrismaClient],
 })
 export class ProjectModule {}
