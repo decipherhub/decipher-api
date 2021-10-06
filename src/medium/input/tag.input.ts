e30fac9a6f9d102c60877e5bcb1152b40fcd6555
hyemmie <lhm0710@naver.com>
2021-10-06 18:02:33 +0900

    add tag input

--- src/medium/input/tag.input.ts
+++ src/medium/input/tag.input.ts
@@ -1,7 +1,13 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 
 @InputType()
 export class TagCreateInput {
   @Field((_type) => String)
   name: string;
+}
+
+@InputType()
+export class TagUniqueInput {
+  @Field((_type) => Int)
+  id: number;
 }
