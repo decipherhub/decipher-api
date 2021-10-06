403248607c40468296976518669d239c824c6206
hyemmie <lhm0710@naver.com>
2021-10-06 18:03:07 +0900

    feat medium module

--- src/medium/medium.module.ts
+++ src/medium/medium.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { MediumResolver } from './medium.resolver';
 import { MediumService } from './medium.service';
 
 @Module({
-  providers: [MediumService]
+  providers: [MediumService, MediumResolver, PrismaClient],
 })
 export class MediumModule {}
--- src/medium/medium.service.ts
+++ src/medium/medium.service.ts
@@ -1,4 +1,180 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient, Medium, Tag } from '@prisma/client';
+import {
+  MediumInput,
+  MediumFindManyInput,
+  MediumUniqueInput,
+  MediumUpdateInput,
+} from './input/medium.input';
+import { TagCreateInput, TagUniqueInput } from './input/tag.input';
 
 @Injectable()
-export class MediumService {}
+export class MediumService {
+  constructor(private prisma: PrismaClient) {}
+
+  async createMedium(mediumCreateInput: MediumInput): Promise<Medium> {
+    const { title, url, imageUrl, summary, memberIds, tagIds } =
+      mediumCreateInput;
+
+    const memberIdList = memberIds.map((id, _index) => {
+      return { memberId: id };
+    });
+
+    const tagIdList = tagIds.map((id, _index) => {
+      return { tagId: id };
+    });
+    const medium = await this.prisma.medium.create({
+      data: {
+        title,
+        url,
+        imageUrl,
+        summary,
+        authors: { createMany: { data: memberIdList } },
+        tags: { createMany: { data: tagIdList } },
+      },
+      include: {
+        authors: {
+          select: {
+            medium: true,
+            member: true,
+          },
+        },
+        tags: true,
+      },
+    });
+
+    return medium;
+  }
+
+  async findMediumById(mediumUniqueInput: MediumUniqueInput): Promise<Medium> {
+    const { id } = mediumUniqueInput;
+
+    const medium = await this.prisma.medium.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        authors: {
+          select: {
+            medium: true,
+            member: true,
+          },
+        },
+        tags: true,
+      },
+    });
+
+    return medium;
+  }
+
+  async findMediums(
+    mediumFindManyInput: MediumFindManyInput,
+  ): Promise<Medium[]> {
+    const { tagId, offset, page } = mediumFindManyInput;
+
+    const mediums = await this.prisma.medium.findMany({
+      skip: page * offset ? page * offset : 0,
+      take: offset,
+      where: {
+        tags: {
+          every: {
+            tagId,
+          },
+        },
+      },
+      include: {
+        authors: {
+          select: {
+            medium: true,
+            member: true,
+          },
+        },
+        tags: true,
+      },
+    });
+
+    return mediums;
+  }
+
+  async updateMedium(mediumUpdateInput: MediumUpdateInput) {
+    const { id, data } = mediumUpdateInput;
+
+    const memberIdList = data.memberIds.map((id, _index) => {
+      return { memberId: id };
+    });
+
+    const tagIdList = data.tagIds.map((id, _index) => {
+      return { tagId: id };
+    });
+
+    const medium = this.prisma.medium.update({
+      where: {
+        id,
+      },
+      data: {
+        title: data.title,
+        url: data.url,
+        imageUrl: data.imageUrl,
+        summary: data.summary,
+        authors: { deleteMany: {}, createMany: { data: memberIdList } },
+        tags: { deleteMany: {}, createMany: { data: tagIdList } },
+      },
+      include: {
+        authors: {
+          select: {
+            medium: true,
+            member: true,
+          },
+        },
+        tags: true,
+      },
+    });
+
+    return medium;
+  }
+
+  async deleteMedium(mediumUniqueInput: MediumUniqueInput): Promise<Medium> {
+    const { id } = mediumUniqueInput;
+    const medium = await this.prisma.medium.delete({
+      where: {
+        id,
+      },
+      include: {
+        authors: {
+          select: {
+            medium: true,
+            member: true,
+          },
+        },
+        tags: true,
+      },
+    });
+
+    return medium;
+  }
+
+  async findTags(): Promise<Tag[]> {
+    const tags = await this.prisma.tag.findMany();
+    return tags;
+  }
+
+  async createTag(tagCreateInput: TagCreateInput): Promise<Tag> {
+    const { name } = tagCreateInput;
+    const tag = await this.prisma.tag.create({
+      data: {
+        name,
+      },
+    });
+
+    return tag;
+  }
+
+  async deleteTag(tagUniqueInput: TagUniqueInput): Promise<Tag> {
+    const { id } = tagUniqueInput;
+    const tag = await this.prisma.tag.delete({
+      where: { id },
+    });
+
+    return tag;
+  }
+}
