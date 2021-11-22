01ed4036b8773a5ea62354d8d9a0d65fcf2bca88
swc0620 <pinetreewave@naver.com>
2021-11-23 05:01:16 +0900

    modified deference input & response and erased deferenceImage

--- src/deference/input/deference.input.ts
+++ src/deference/input/deference.input.ts
@@ -1,13 +1,57 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
-import { CreateDeferenceImageInput, UpdateDeferenceImageInput } from './deferenceImage.input';
+import { CreateDeferencePartnerLogoUrlInput, UpdateDeferencePartnerLogoUrlInput } from 'deference/input/deferencePartnerLogoUrl.input';
+import { CreateDeferenceReferenceInput, UpdateDeferenceReferenceInput } from 'deference/input/deferenceReference.input';
+import { CreateDeferenceSpeakerInput, UpdateDeferenceSpeakerInput } from 'deference/input/deferenceSpeaker.input';
 
 @InputType()
 export class CreateDeferenceInput {
   @Field((type) => Int)
   year: number;
 
-  @Field((type) => [CreateDeferenceImageInput], { nullable: true })
-  deferenceImage: CreateDeferenceImageInput[];
+  @Field()
+  summary: string;
+
+  @Field((type) => Date)
+  startTime: Date;
+
+  @Field((type) => Date)
+  endTime: Date;
+
+  @Field()
+  participationMethod: string;
+
+  @Field()
+  contents: string;
+
+  @Field()
+  audience: string;
+
+  @Field()
+  host: string;
+
+  @Field()
+  formLink: string;
+
+  @Field()
+  mainPosterUrlDesktop: string;
+
+  @Field()
+  mainPosterUrlMobile: string;
+
+  @Field()
+  schedulePosterUrlDesktop: string;
+
+  @Field()
+  schedulePosterUrlMobile: string;
+
+  @Field((type) => [CreateDeferenceSpeakerInput], { nullable: true })
+  deferenceSpeaker: CreateDeferenceSpeakerInput[];
+
+  @Field((type) => [CreateDeferenceReferenceInput], { nullable: true })
+  deferenceReference: CreateDeferenceReferenceInput[];
+
+  @Field((type) => [CreateDeferencePartnerLogoUrlInput], { nullable: true })
+  deferencePartnerLogoUrl: CreateDeferencePartnerLogoUrlInput[];
 }
 
 @InputType()
@@ -18,6 +62,48 @@ export class UpdateDeferenceInput {
   @Field((type) => Int)
   year: number;
 
-  @Field((type) => UpdateDeferenceImageInput, { nullable: true })
-  deferenceImage: UpdateDeferenceImageInput;
+  @Field()
+  summary: string;
+
+  @Field((type) => Date)
+  startTime: Date;
+
+  @Field((type) => Date)
+  endTime: Date;
+
+  @Field()
+  participationMethod: string;
+
+  @Field()
+  contents: string;
+
+  @Field()
+  audience: string;
+
+  @Field()
+  host: string;
+
+  @Field()
+  formLink: string;
+
+  @Field()
+  mainPosterUrlDesktop: string;
+
+  @Field()
+  mainPosterUrlMobile: string;
+
+  @Field()
+  schedulePosterUrlDesktop: string;
+
+  @Field()
+  schedulePosterUrlMobile: string;
+
+  @Field((type) => UpdateDeferenceSpeakerInput, { nullable: true })
+  deferenceSpeaker: UpdateDeferenceSpeakerInput;
+
+  @Field((type) => UpdateDeferenceReferenceInput, { nullable: true })
+  deferenceReference: UpdateDeferenceReferenceInput;
+
+  @Field((type) => UpdateDeferencePartnerLogoUrlInput, { nullable: true })
+  deferencePartnerLogoUrl: UpdateDeferencePartnerLogoUrlInput;
 }
